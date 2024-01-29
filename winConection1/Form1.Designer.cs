fed2342eac54558953a553f9f3c7802ae2d5ca58
LabP4E010 <LabP4E010@1-20-04-E010-19>
2024-01-29 16:38:50 -0500

    Agregar archivos de proyecto.

---
+++ winConection1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace winConection1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.lstConexion = new System.Windows.Forms.ListBox();
+            this.lblConn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutar.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnEjecutar.Location = new System.Drawing.Point(340, 125);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(111, 54);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // lstConexion
+            // 
+            this.lstConexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstConexion.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lstConexion.FormattingEnabled = true;
+            this.lstConexion.ItemHeight = 16;
+            this.lstConexion.Location = new System.Drawing.Point(32, 54);
+            this.lstConexion.Name = "lstConexion";
+            this.lstConexion.Size = new System.Drawing.Size(208, 308);
+            this.lstConexion.TabIndex = 1;
+            // 
+            // lblConn
+            // 
+            this.lblConn.AutoSize = true;
+            this.lblConn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.lblConn.Location = new System.Drawing.Point(244, 9);
+            this.lblConn.Name = "lblConn";
+            this.lblConn.Size = new System.Drawing.Size(148, 13);
+            this.lblConn.TabIndex = 2;
+            this.lblConn.Text = "Conexión a BD Usando Using";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Magenta;
+            this.label1.Location = new System.Drawing.Point(106, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clientes";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Red;
+            this.btnSalir.Location = new System.Drawing.Point(543, 343);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(92, 40);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 395);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblConn);
+            this.Controls.Add(this.lstConexion);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.ListBox lstConexion;
+        private System.Windows.Forms.Label lblConn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ winConection1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace winConection1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public String strConn = "Data Source=(local); Initial Catalog=facturacion; Integrated Security=SSPI";
+        public String strComm = "SELECT nombre_cli, dir_cli FROM Clientes";
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            
+            using(SqlConnection conn = new SqlConnection(strConn)) 
+            { 
+                conn.Open();
+            }
+        }
+    }
+}
